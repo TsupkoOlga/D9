20a1f3e8d39229fd5c27f3bd121d9b91a4f5c2ad
TsupkoOlga <tsupko2000@gmail.com>
2023-03-11 20:16:39 +0600

    Новости закончены

--- pythonProject/NewsPaper/news/urls.py
+++ pythonProject/NewsPaper/news/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import PostsList, PostDetail, PostCreate
+from .views import PostsList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
-    # path — означает путь.
-    # В данном случае путь ко всем публикациям у нас останется пустым,
-    # чуть позже станет ясно почему.
-    # Т.к. наше объявленное представление является классом,
-    # а Django ожидает функцию, нам надо представить этот класс в виде view.
-    # Для этого вызываем метод as_view.
-    path('', PostsList.as_view()),
-    # pk — это первичный ключ публикации, который будет выводиться у нас в шаблон
-    # int — указывает на то, что принимаются только целочисленные значения
+    path('', PostsList.as_view(), name = 'post_list'),
     path('<int:pk>', PostDetail.as_view(), name = 'post_detail'),
     path('create/', PostCreate.as_view(), name = 'post_create'),
+    path('<int:pk>/edit/', PostUpdate.as_view(), name='post_update'),
+    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
 ]
--- pythonProject/NewsPaper/news/views.py
+++ pythonProject/NewsPaper/news/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .filters import PostFilter
 from .forms import PostForm
@@ -44,7 +45,16 @@ class PostDetail(DetailView):
 class PostCreate(CreateView):
     form_class = PostForm
     model = Post
-    template_name = 'product_edit.html'
+    template_name = 'post_edit.html'
 
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
 
 
