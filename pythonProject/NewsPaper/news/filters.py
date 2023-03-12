6647a7dee38ee9b40659ada07fa47d79a2ef1992
TsupkoOlga <tsupko2000@gmail.com>
2023-03-13 01:47:30 +0600

    Уф

--- pythonProject/NewsPaper/news/filters.py
+++ pythonProject/NewsPaper/news/filters.py
@@ -1,32 +1,29 @@
+import django_filters
 from django_filters import FilterSet, ModelMultipleChoiceFilter
 from .models import Post, User, Category
 
 
-# Создаем свой набор фильтров для модели Post.
-# FilterSet, который мы наследуем,
-# должен чем-то напомнить знакомые вам Django дженерики.
 class PostFilter(FilterSet):
-   author = ModelMultipleChoiceFilter(
-      field_name = 'author__user',
-      queryset = User.objects.all(),
-      label = 'Author',
-   )
+    author = ModelMultipleChoiceFilter(
+        field_name = 'author__user',
+        queryset = User.objects.all(),
+        label = 'Author',
+    )
+    time_in = django_filters.DateFilter(
+        lookup_expr='gt',
+        label = 'Start date',
+    )
 
-   # category = ModelMultipleChoiceFilter(
-   #    field_name = 'postcategory__category',
-   #    queryset = Category.objects.all(),
-   #    label = 'Category',
-   #)
 
-   class Meta:
-       # В Meta классе мы должны указать Django модель,
-       # в которой будем фильтровать записи.
-       model = Post
-       # В fields мы описываем по каким полям модели
-       # будет производиться фильтрация.
-       fields = {
-           # поиск по названию
-           'title': ['icontains'],
-           # время должно быть больше или равно
-           # 'quantity': ['gt'],
-       }
+    # category = ModelMultipleChoiceFilter(
+    #    field_name = 'postcategory__category',
+    #    queryset = Category.objects.all(),
+    #    label = 'Category',
+    #)
+
+    class Meta:
+        model = Post
+        fields = {
+            'title': ['icontains'],
+             # 'time_in': ['gt'],
+        }
